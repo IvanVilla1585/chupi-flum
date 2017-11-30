d7fcf872101b4f6a509b14aea43e167ec21778d4
Ivan Villa <trezeguet55@gmail.com>
2017-11-30 09:00:00 -0500

    <categories model.js>
    create mongoose schema

---
+++ services/categories/src/lib/model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const CODES = ['PRODUCT', 'MATERIAL'];
+
+const schema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true,
+	},
+	code: {
+		type: String,
+		enum: CODES,
+		required: true,
+		unique: true,
+		uppercase: true,
+		index: true
+	},
+	status: {
+		type: Boolean,
+		default: true
+	}
+}, { timestamps: true });
+
+if (!schema.options.toJSON) schema.options.toJSON = {};
+
+/**
+ * Add a tranforma method to change _id by id
+ * whent toJSON is used.
+ */
+schema.options.toJSON.transform = (doc, ret) => {
+	// remove the _id of every document before returning the result
+	ret.id = ret._id; // eslint-disable-line no-param-reassign
+	delete ret._id; // eslint-disable-line no-param-reassign
+	return ret;
+};
+
+module.exports = mongoose.model('Category', schema);
+
