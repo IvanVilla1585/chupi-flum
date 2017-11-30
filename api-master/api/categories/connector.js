1054f6dc74c012a343b31c926e4a2c3c96eb6798
Ivan Villa <trezeguet55@gmail.com>
2017-11-30 09:12:03 -0500

    <categories connector.js>
    create microservice connector

---
+++ api-master/api/categories/connector.js
@@ -0,0 +1,13 @@
+// @flow
+import BaseConnector from '../helpers/BaseConnector';
+
+/**
+ * Exteds of BaseConnector, obtains the base methods to fetch * resources given the url
+ * @extends BaseConnector
+ * @class
+ */
+export default class CategoryConnector extends BaseConnector {
+  constructor(API_ROOT: string = '') {
+    super(API_ROOT);
+  }
+}
