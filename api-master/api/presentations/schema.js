40296a32bdd04590ca54eb9f0a254ce08f2df8fc
Ivan Villa <trezeguet55@gmail.com>
2017-12-02 06:44:19 -0500

    <presentations schema.js>
    create graphql schema

---
+++ api-master/api/presentations/schema.js
@@ -0,0 +1,55 @@
+
+const schema = [`
+
+  enum STATUS_PRE {
+    ACTIVE,
+    INACTIVE
+  }
+
+  # data to create presentation
+  input PresentationInput {
+    # presentation name
+    name: String!,
+    # presentation description
+    description: String
+  }
+  
+  # data to create presentation
+  input PresentationEditInput {
+    # presentation name
+    name: String,
+    # presentation description
+    description: String
+  }
+  
+  type Presentation {
+    id: ID,
+    # presentation name
+    name: String,
+    # presentation description
+    description: String,
+    # presentation status
+    status: STATUS_PRE,
+    createdAt: String
+  }
+  
+  extend type Query {
+    # fetch all presentation
+    presentations(status: STATUS): [Presentation]
+    # find presentation by id
+    presentationById(id: ID!): Presentation
+  }
+  
+  extend type Mutation {
+    # create presentation
+    presentationAdd(data: PresentationInput): Presentation
+    # update presentation
+    presentationEdit(id: ID!, data: PresentationEditInput): Presentation
+    # delete presentation
+    presentationDelete(id: ID): Presentation
+    # update status presentation
+    presentationStatus(id: ID!, status:   String): Presentation
+  }
+`];
+
+export default schema;
