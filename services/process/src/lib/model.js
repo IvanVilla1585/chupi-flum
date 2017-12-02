5fa06f8d81e378451ce8762cc24ba980a904329f
Ivan Villa <trezeguet55@gmail.com>
2017-12-02 15:54:23 -0500

    <process model.js>
    create mongoose schema

---
+++ services/process/src/lib/model.js
@@ -0,0 +1,45 @@
+
+
+const mongoose = require('mongoose');
+
+const STATUS = ['ACTIVE', 'INACTIVE'];
+
+const schema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	description: {
+		type: String
+	},
+	time: {
+		type: String,
+		required: true
+	},
+	machine: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Machine',
+		required: true
+	},
+	status: {
+		type: String,
+		enum: STATUS,
+		default: STATUS[0]
+	}
+});
+
+if (!schema.options.toJSON) schema.options.toJSON = {};
+
+/**
+ * Add a tranforma method to change _id by id
+ * whent toJSON is used.
+ */
+schema.options.toJSON.transform = (doc, ret) => {
+	// remove the _id of every document before returning the result
+	ret.id = ret._id; // eslint-disable-line no-param-reassign
+	delete ret._id; // eslint-disable-line no-param-reassign
+	return ret;
+};
+
+module.exports = mongoose.model('Process', schema);
